04ccb41094c8b93f3958bd52b718ac724ea4ba1f
KJYoung <jykim157@naver.com>
2021-01-23 22:42:32 +0900

    Mobile Size modify 2
    .

--- bgMobile.js
+++ bgMobile.js
@@ -10,7 +10,7 @@ function paintImage(imgNumber){
     const image = new Image();
     image.src = `images/bg${imgNumber}.jpg`;
     image.style.width = "100vw";
-    image.style.height = "200vh";
+    image.style.height = "100vh";
     image.style.backgroundRepeat = "repeat-y";
     image.style.backgroundPosition = "center center";
     image.classList.add('bgImage');
